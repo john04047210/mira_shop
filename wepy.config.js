005d3503a2247697343b64906dde922e6a13c1e2
Peng <john_04047210@163.com>
2018-12-10 16:02:21 +0900

    Update wepy.config.js

--- wepy.config.js
+++ wepy.config.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   appConfig: {
     noPromiseAPI: ['createSelectorQuery'],
-    baseUrl: process.env.NODE_ENV === 'production' ? 'https://wepy.shilikaif.com' : 'http://wepy.shilikaif.com
+    baseUrl: process.env.NODE_ENV === 'production' ? 'https://wepy.shilikaif.com' : 'http://wepy.shilikaif.com'
   }
 }
 
